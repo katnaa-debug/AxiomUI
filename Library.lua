bbbd35e4972c0512fc8cd4e5e147ff91c3a022d0
katnaa-debug <vladbumaga228d@gmail.com>
2026-09-22 18:05:00 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2433,7 +2433,8 @@ function Library.CreateWindow(config)
 
 			local rgbLayout = Instance.new("UIListLayout")
 			rgbLayout.FillDirection = Enum.FillDirection.Horizontal
-			rgbLayout.HorizontalAlignment = Enum.HorizontalAlignment.SpaceBetween
+			rgbLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+			rgbLayout.Padding = UDim.new(0, 8)
 			rgbLayout.SortOrder = Enum.SortOrder.LayoutOrder
 			rgbLayout.Parent = rgbContainer
 
@@ -7135,7 +7136,7 @@ function Library.CreateWindow(config)
 						elseif Window._themeElements[id] then
 							Window._themeElements[id].API:Load(val)
 						end
-					en
+					end
 					Window:Notify({
 						Title = "Theme System",
 						Description = "Auto-loaded theme: " .. tAutoName,
